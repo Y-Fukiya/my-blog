429ea3dc8decc6b8d3614e97d9bac6b25ba16172
Y-Fukiya <yoshihiro.fukiya@gmail.com>
2024-01-16 23:42:16 +0900

    init

---
+++ src/lib/config/site.ts
@@ -0,0 +1,17 @@
+import type { SiteConfig } from '$lib/types/site'
+
+export const site: SiteConfig = {
+  protocol: import.meta.env.URARA_SITE_PROTOCOL ?? import.meta.env.DEV ? 'http://' : 'https://',
+  domain: import.meta.env.URARA_SITE_DOMAIN ?? 'urara-demo.netlify.app',
+  title: 'コードで綴る道楽人生',
+  subtitle: 'Sweet & Powerful SvelteKit Blog Template',
+  lang: 'en-US',
+  description: 'Powered by SvelteKit/Urara',
+  author: {
+    avatar: '/assets/maskable@512.png',
+    name: 'ふっきゃん',
+    status: '🌸',
+    bio: 'lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+  },
+  themeColor: '#3D4451'
+}
